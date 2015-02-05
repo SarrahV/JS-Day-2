042e89a7c4a5e44a7d829dc2dd0ecdda1fa76d0f
Sarrah Vesselov <sarchka1@gmail.com>
2015-02-04 21:46:27 -0500

    completed 7

--- second-try.js
+++ second-try.js
@@ -128,19 +128,16 @@ function letterIndex(myLetter) {
 // HINT: use an array and the modulo (%) operator
 
 
-function reverseLetterIndex(letterNum) {
-  var alph = ["a", "b", "c", "d", "e", 
-              "f", "g", "h", "i", "j", 
-              "k", "l", "m", "n", "o", 
-              "p", "q", "r", "s", "t", 
-              "u", "v", "w", "x", "y",
-              "z"];
-  var index;
-  index = 0;
-
+function reverseLetterIndex(myLetter) {
+  var alph, letter;
+  var alph = "abcdefghijklmnopqrstuvwxyz".split("");
+  
+  letter = alph[myLetter];
+    
+  return letter;
 }
 
-return letter;
+
 
 //---------
 
